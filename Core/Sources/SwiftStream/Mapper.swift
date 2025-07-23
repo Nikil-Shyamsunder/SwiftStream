ec92d43a92fd833194ed46a4b19b2f3f2c9337ed
Nikil-Shyamsunder <nikil.shyamsunder@gmail.com>
2025-07-23 16:21:32 -0400

    introduce mapper protocol

---
+++ Core/Sources/SwiftStream/Mapper.swift
@@ -0,0 +1,22 @@
+//
+//  MapperContext.swift
+//  SwiftStream
+//
+//  Created by Nikil Shyamsunder 2025
+//
+
+import Foundation
+
+public protocol Mapper {
+    // Helpful source: https://medium.com/globant/swift-generics-and-associated-types-73aa2b184c7a
+    // associated types are sort of like generics but for protocols
+    // we want to use CustomStringConvertible since we interface with Hadoop via standard I/O operations
+    associatedtype KOut: CustomStringConvertible
+    associatedtype VOut: CustomStringConvertible
+    
+    // serves like the setup() function in Java MapReduce
+    init()
+    // standard map() function in MapReduce
+    func map(key: String, value: String, ctx: MapperContext<KOut, VOut>)
+}
+
